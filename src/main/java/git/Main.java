2a82cb7ff2b3c0cce66eb9478bd4a050de6d36a3
shiyaobo <34913501+shiyaobo@users.noreply.github.com>
2017-12-29 09:08:26 +0800

    Update Main.java

--- src/main/java/git/Main.java
+++ src/main/java/git/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("添加代码");
-		System.out.println("在出上传");
+		System.out.println("在次上传");
 	}
 }
